19a06a79ad4193180deee7656d445734f27c253c
prayansh <parmarprayansh2@gmail.com>
2024-07-21 21:08:12 +0530

    Fix null to undefined

--- components/ui-components/avatar-provider.tsx
+++ components/ui-components/avatar-provider.tsx
@@ -9,9 +9,9 @@ interface UserAvatarProps {
 
 export const AvatarProvider = ({ src, className, name }: UserAvatarProps) => {
   return (
-    <Avatar className={cn("h-7 w-7 md:h-10  md:w-10  ", className)}>
-      <AvatarImage src={src} />
-      <AvatarFallback>{name}</AvatarFallback>
+    <Avatar className={cn("h-7 w-7 md:h-10 md:w-10", className)}>
+      <AvatarImage src={src ?? undefined} />
+      <AvatarFallback>{name ?? undefined}</AvatarFallback>
     </Avatar>
   );
 };
